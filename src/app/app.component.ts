6efb420a10d0c612403611a05a63427b14efaab7
Carlos Rogério Campos Anselmo <rogerio.anselmo@lambda3.com.br>
2020-02-18 17:15:20 -0300

    aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+
+  title = "Alura Teams";
+
+  photos = [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Clube_do_Remo_S%C3%ADmbolo.svg/539px-Clube_do_Remo_S%C3%ADmbolo.svg.png",
+      Name: "Remo"
+    },
+    {
+      url: "https://img.favpng.com/8/19/7/fc-barcelona-museum-fc-barcelona-handbol-fc-barcelona-femen-uefa-champions-league-png-favpng-KtW92tpHPkEzRPH0EqTG8qZzh_t.jpg",
+      Name: "Barça"
+    },
+    {
+      url: "https://a.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F86.png",
+      Name: "Real"
+    }
+  ]
 }
